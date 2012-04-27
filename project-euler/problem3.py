b195953f7fd220ecee7432a53829d2d3b4f21f98
Nathan Starr <nathanstarr@gmail.com>
2012-04-27 05:02:54 -0400

    Adding some completed problems

---
+++ project-euler/problem3.py
@@ -0,0 +1,12 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143 ?
+
+i = 2
+n = 600851475143
+while i * i < n:
+	if n % i == 0:
+		n = n /i
+	i = i + 1
+
+print n 
